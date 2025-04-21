8ee021301931a7ab6947e564ec97d09f0ea6f966
Sandra PS <sandraps58038@gmail.com>
2025-04-21 11:47:52 +0530

    frontend completed

--- server.js
+++ server.js
@@ -29,16 +29,10 @@ import daybookRoutes from './routes/daybook.js';
 app.use("/api/daybook", daybookRoutes);
 
 
-import userRoutes from "./routes/userRoutes.js";
-app.use("/api/users", userRoutes);
-
-import dashboardRoutes from "./routes/dashboardRoutes.js"
-app.use("/api/dashboard", dashboardRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ Database Connected"))
   .catch(err => console.log("❌ DB Error:", err));
 
