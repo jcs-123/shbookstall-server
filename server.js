1320615d8f88ef2e17cc10f9a3763cb2b3b37c5c
Sandra PS <sandraps58038@gmail.com>
2025-04-21 10:28:21 +0530

    server code changed

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Database Connected"))
-  .catch((err) => console.error("❌ DB Connection Error:", err));
-  
+  .catch(err => console.log("❌ DB Error:", err));
+
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
