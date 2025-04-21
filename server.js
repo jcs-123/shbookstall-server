940ffc28dad59227dd2103fe8700fd36108ed0d6
Sandra PS <sandraps58038@gmail.com>
2025-04-21 10:25:30 +0530

    server package changes

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("✅ Database Connected"))
-  .catch(err => console.log("❌ DB Error:", err));
-
+  .catch((err) => console.error("❌ DB Connection Error:", err));
+  
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
